8d951cb95816a7b88e01292cabe82abf84465841
Krishnesha <krishneshasarma@gmail.com>
2026-06-22 21:46:23 +0530

    variables.cpp

--- basics/cpp2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout<<"HELLO PW \n";
-    cout<<"HELLO CW";
-
-    return 0;
-    
-}
---
+++ basics/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    x = 16;
+    int y;
+    y = 12;
+    cout<<x*y;
+}
+
+
